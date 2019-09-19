5a9af373de57d7d16460a99032cacb4766263401
Jere Virta <jerenius@gmail.com>
2019-09-19 08:44:54 +0300

    .

--- content/opt/start.sh
+++ content/opt/start.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 
-
-#node-red --settings /settings.js &
-
-#/usr/src/app/temperature.py
-
 ## Start supervisord
 /usr/bin/supervisord -c /etc/supervisor/supervisord.conf -n &
 trap "supervisorctl shutdown && wait" SIGTERM
