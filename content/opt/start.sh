12b9bd2156405ffa997578f7ba3f07271e2b69cc
Jere Virta <jerenius@gmail.com>
2019-09-19 15:45:06 +0300

    .

--- content/opt/start.sh
+++ content/opt/start.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 
+## Create LLDPd configuration
+cat <<EOL >>/etc/lldpd.conf
+configure system hostname $HOSTNAME
+configure system interface pattern eth0
+configure med fast-start enable
+EOL
+
+
 ## Start supervisord
 /usr/bin/supervisord -c /etc/supervisor/supervisord.conf -n &
 trap "supervisorctl shutdown && wait" SIGTERM
